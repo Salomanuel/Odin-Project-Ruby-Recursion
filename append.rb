88dbc873863175aa36e447039a4f6b3e61bd9e15
Manuel <salomanuel.aux+code@gmail.com>
2016-09-23 17:10:11 +0200

    Add reverse append exercise

--- append.rb
+++ append.rb
@@ -1,8 +1,20 @@
 def append( ary, n )
-	return ary << 0 if n == 0
+	return ary if n < 0
 	ary << n
 	append(ary, n-1)
+	return ary
 end
 
-puts append([], 2).join(", ") # => [2, 1, 0]
-puts append([], 3).join(", ") # => [3, 2, 1, 0]
+
+def reverse_append(ary, n)
+	return ary if n < 0
+	reverse_append(ary, n-1)
+	ary << n
+	return ary
+end
+
+puts append([], 2).join(", ") # =>  (2,1,0)
+puts append([], 3).join(", ") # =>  (3,2,1,0)
+puts "*" * 40
+puts reverse_append([], 1).join(", ")	# => (0,1)
+puts reverse_append([], 2).join(", ")	# => (0,1,2)
