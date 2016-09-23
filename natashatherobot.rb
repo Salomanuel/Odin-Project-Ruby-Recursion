111b96e5037c10bf83b9e83a65cf4bda8cd53994
Manuel <salomanuel.aux+code@gmail.com>
2016-09-23 19:55:36 +0200

    natashathecoso finished

--- natashatherobot.rb
+++ natashatherobot.rb
@@ -8,5 +8,28 @@ def factorial_recursion(n)
 end
 
 puts factorial(5)
-
 puts factorial_recursion(5)
+puts "*" * 30
+
+def fibonacci(n)
+	sequence = []
+	(0..n).each do |n|
+		if n < 2
+			sequence << n
+		else 
+			sequence << sequence[-1] + sequence[-2]
+		end
+	end
+	sequence.last
+end
+
+def fibonacci_rec(n)
+	if n < 2
+		n
+	else
+		fibonacci_rec(n-1) + fibonacci(n-2)
+	end
+end
+
+puts fibonacci(7)
+puts fibonacci_rec(7)
