5593aff73faa354018b219e25aacbb42a875b698
Manuel <salomanuel.aux+code@gmail.com>
2016-10-08 16:57:10 +0200

    rosettacode mergesort is AWESOME

--- rosettacode_mergesort.rb
+++ rosettacode_mergesort.rb
@@ -4,9 +4,16 @@ def merge_sort(m)
 	middle = m.length / 2
 	left   = merge_sort(m[0...middle])
 	right  = merge_sort(m[middle..-1])
-	# merge(left, righ)		# LAST ORIGINAL LINE
-	puts "#{left}**#{right}"
-	right
+	merge(left, right)		# LAST ORIGINAL LINE
+	# puts "#{left}**#{right}"
+end
+
+def merge(left, right)
+	result = []
+	until left.empty? or right.empty?
+		result << (left.first <= right.first ? left.shift : right.shift)
+	end
+	result + left + right
 end
 
 def randi(qty, range=100)
@@ -15,6 +22,6 @@ def randi(qty, range=100)
 	return list
 end
 
-puts merge_sort([43,24,11,33,31])
-# puts merge_sort(randi(10))
+# puts merge_sort([43,24,11,33,31]).join(", ")
+puts merge_sort(randi(20)).join(", ")
 # puts randi(10)
